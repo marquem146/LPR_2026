4642d4500cc7fff6b5e12e8a41ad33b9dbdef0b3
marquem146 <fsouzasrs@gmail.com>
2026-06-16 10:02:30 -0300

    aula08

---
+++ aula08/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+class aula08
+{
+    public struct filmes
+    {
+        public string titulo;
+        public string diretor;
+        public int lancamento;
+    }
+    static void main()
+    {   for(int i = 0; i < 3; i++)
+        {
+         filmes[] datafilmes1 = new filmes[3];
+        filmes[] datafilmes2 = new filmes[3];
+        filmes[] datafilmes3 = new filmes[3];
+        datafilmes3[3];
+        Console.WriteLine("digite os dados do primeiro filme: ");
+        titulo1 = Console.Read(titulo[i]);
+        diretor1 = Console.Read(Diretor[i]);
+        lancamento1 = Console.Read(lancamento[i]);
+        Console.WriteLine("digite os dados do segundo filme: ");
+        titulo2 =  Console.Read(titulo2[i]);
+        diretor2 = Console.Read(Diretor[i]);
+        lancamento2 = Console.Read(lancamento[i]);
+        Console.WriteLine("digite os dados do terceiro filme: ");
+        titulo3 =  Console.Read(titulo3[i]);
+        diretor3 = Console.Read(Diretor[i]);
+        lancamento3 = Console.Read(lancamento[i]);   
+        }
+        data
+        foreach(var f in datafilmes1)
+        {
+          Console
+        }
+        foreach(var f in datafilmes2)
+        {
+            
+        }
+    foreach(var f in datafilmes3)
+        {
+            
+        }
+    }
+}
